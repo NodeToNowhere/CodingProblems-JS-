361c14b45f4c8ae414204896b77cd77a5b57a225
Tony <NodeToNowhere@gmail.com>
2022-04-02 21:09:21 +0100

    Capitalise

--- capitalize/index.js
+++ capitalize/index.js
@@ -7,6 +7,16 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  let result = str[0].toUpperCase();
 
-module.exports = capitalize;
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === ' ') {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+
+  return result;
+}
